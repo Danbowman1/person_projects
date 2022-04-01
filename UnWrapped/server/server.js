535d76fbb151b24ced079c141f1086a37d61f8f2
Mahmoud Khudairi <mahmoudkhudairi@gmail.com>
2022-04-01 03:33:34 +0300

    refactor backend

--- UnWrapped/server/server.js
+++ UnWrapped/server/server.js
@@ -1,64 +1,12 @@
-const express = require('express')
-const multer = require('multer')
-const fs = require('fs');
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, 'uploads')
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, file.fieldname + '-' + Date.now())
-//     }
-// })
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+require('./config/mongoose.config');
+require('./routes/cigar.routes')(app);
+const port = 8000;
 
-// const upload = multer({storage: storage})
-
-
-
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors())
-
-app.use('/uploads', express.static('uploads'));
-
-app.use(function (err, req, res, next) {
-    console.log('This is the invalid field ->', err.field);
-    next(err);
-});
-
-require('./config/mongoose.config')
-require('./routes/cigar.routes')(app)
-
-
-
-
-
-// app.post(
-//     '/uploadpicture',
-//     upload.array('pictureList'),
-//     function (req, res, next) {
-//         console.log(req.files);
-//         console.log("req.files.path", req.files[0].path)
-//         fs.rename(
-//             req.files[0].path,
-//             'uploads/' + req.files[0].originalname,
-//             function (err) {
-//                 if (err) {
-//                     res.send('Error in file upload');
-//                 } else {
-//                     res.send('File upload was a success!');
-//                 }
-//             }
-//         );
-//     }
-// );
-
-
-
-
-const port = 8000
-
-app.listen(port, () => console.log(`Listening on port: ${port}`))
+app.listen(port, () => console.log(`Listening on port: ${port}`));
